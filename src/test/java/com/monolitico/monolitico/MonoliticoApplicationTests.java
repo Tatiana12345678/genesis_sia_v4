f0aa9d0c3d8ca00b7e0c57f7a86fe41e303e0957
Tatiana Zambrano <tpzambranov@ufpso.edu.co>
2024-03-15 19:02:00 -0500

    primer version del proyecto

---
+++ src/test/java/com/monolitico/monolitico/MonoliticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monolitico.monolitico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoliticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
